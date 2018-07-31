cb83d81385cc838ea3dbdc9922b9337492a89be0
sadeghieh <sadeghieh@gmail.com>
2018-08-01 00:19:36 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2013 Simon Fraser University Library
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @brief Wrapper for email logger plugin.
+ * @package plugins.generic.emailLogger
+ *
+ */
+require_once('plugins/generic/mandrillMailer/MandrillMailPlugin.inc.php');
+
+return new MandrillMailPlugin();
+
+?>
